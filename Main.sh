107e5803d9455a1fcd157e2877db28c9331f8977
Prabhakar <prabhakar.9885@gmail.com>
2016-04-02 16:28:48 +0530

    Updated Driver code

--- Main.sh
+++ Main.sh
@@ -2,9 +2,9 @@
 #
 # Usage:
 # ======
-# bash Main.sh <path to folder containing the docs that are to be summarized> 
+# bash Main.sh <path to folder containing the docs that are to be summarized> <No. of Clusters> <Summary size in words>
 #
-# eg.,	bash Main.sh ../IRE/Project/TEST_docs_Parsed/d30001t/
+# eg.,	bash Main.sh ../IRE/Project/TEST_docs_Parsed/d30001t/ 3 300
 #
 ###########################################################################
 
@@ -13,6 +13,7 @@
 
 dirName="$1"
 noOfClusters="$2"
+summarySize="$3"
 
 if [[ -d "$dirName" ]]; then
 	echo -n "Generating tfid scores..."
@@ -22,7 +23,7 @@ if [[ -d "$dirName" ]]; then
 	python k_means.py "$dirName" "$noOfClusters" > /dev/null
 	echo "done"
 	echo -n "Generating Clusters..."
-	python Generate_Summary.py "$dirName" > "$dirName.summary"
+	python Generate_Summary.py "$dirName" "$summarySize"> "$dirName.summary"
 	echo "done"
 else
 	echo "Director not found: $dirName"
