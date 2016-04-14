f7d00989129435c19b757f641841ea94dd3fdc61
Prabhakar <prabhakar.9885@gmail.com>
2016-04-14 18:20:32 +0530

    Updated the script

--- processAllData.sh
+++ processAllData.sh
@@ -3,18 +3,24 @@
 inputFolder="../TEST_docs_Parsed/"
 humanSummary="../Test_Summaries/"
 dirlist=($(ls -1 "$inputFolder"))
-algo=3
+algoList=(1 2 3)
 
-for dir in ${dirlist[*]}
+for algo in "${algoList[@]}"
 do
-	echo "Processing "$dir
-	dirName="$inputFolder$dir"
-	bash Main.sh "$dirName" $algo 3
-	echo "----------------------------------------"
-	summaryFile="$dirName.summary"
-	humanSummaryFile="$humanSummary$dir"
-	echo $summaryFile
-	echo $humanSummaryFile
-	java -cp ./C_Rouge/C_ROUGE7.jar executiverouge.C_ROUGE7 "$summaryFile"  "$humanSummaryFile"  1 A R >> ROUG1.3
-	rm *.out
+	for dir in ${dirlist[*]}
+	do
+		echo "----------------------------------------"
+		echo "Processing "$dir
+		dirName="$inputFolder$dir"
+		rougFileName="ROUG_"$algo
+		bash Main.sh "$dirName" $algo 3
+		summaryFile="$dirName.summary"
+		humanSummaryFile="$humanSummary$dir"
+		echo $summaryFile
+		echo $humanSummaryFile
+		echo "$rougFileName" 
+		echo -en "\n"$dir" : " >> "$rougFileName"
+		java -cp ./C_Rouge/C_ROUGE7.jar executiverouge.C_ROUGE7 "$summaryFile"  "$humanSummaryFile"  1 A R >> "$rougFileName"
+		rm *.out
+	done
 done
